e2b525e1203ecc921ef0428fabfea86f5ae85a8e
nugmaneg <nugmaneg@gmail.com>
2024-02-18 21:50:21 +0300

    .

---
+++ C#/Lesson4/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 2: Задайте массив заполненный случайными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+*/
+
+Random random = new Random();
+int[] numbers = new int[10];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = random.Next(100, 1000);
+}
+
+int countEven = 0;
+foreach (int number in numbers)
+{
+    if (number % 2 == 0)
+    {
+        countEven++;
+    }
+}
+
+// Console.WriteLine($"{string.Join(", ", numbers)}");
+Console.WriteLine($"Количество чётных чисел в массиве: {countEven}");
---
+++ C#/Lesson4/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 3: Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+*/
+
+int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+for (int i = 0; i < numbers.Length / 2; i++)
+{
+    int temp = numbers[i];
+    numbers[i] = numbers[numbers.Length - 1 - i];
+    numbers[numbers.Length - 1 - i] = temp;
+}
+
+foreach (int number in numbers)
+{
+    Console.Write($"{number} ");
+}
